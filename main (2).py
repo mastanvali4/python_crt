2284afd9561445ad5a44d62178f32ac8a2983604
Mastan Vali <valivali434434@gmail.com>
2025-12-24 12:29:12 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,7 +1,7 @@
 x = int (input())
 for i in range (x):
-    for j in range(x):
-        if i==0 or j==0 or i==x-1 or j==x-1:
+    for j in range(i+1):
+        if i==x-1 or j==0 or i==j:
          print("*",end=" ")
         else: 
           print(" ",end=" ")
